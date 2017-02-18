56ba079e8d16540cd1398657b0c03115e3bfc923
Egg4 <clement5444@yahoo.fr>
2017-02-18 19:36:41 +0100

    Handle error as exception

--- src/Component/Http/Exception.php
+++ src/Component/Http/Exception.php
@@ -24,6 +24,11 @@ class Exception extends AbstractComponent
     public function run(Request $request, Response $response, Component $next)
     {
         try {
+            set_error_handler(function ($err_severity, $err_msg, $err_file, $err_line, array $err_context) {
+                if (0 === error_reporting()) return false;
+                throw new \ErrorException($err_msg, 0, $err_severity, $err_file, $err_line);
+            });
+
             $response = $next($request, $response);
         }
         catch (\Egg\Http\Exception $exception) {
@@ -38,9 +43,15 @@ class Exception extends AbstractComponent
             }
             $response = $this->container['response'] ? $this->container['response'] : $response;
             $response = $response->withStatus(500);
+            if ($this->container['environment']['APP_ENV'] == 'dev') {
+                $message = sprintf('%s in %s:%s', $exception->getMessage(), $exception->getFile(), $exception->getLine());
+            }
+            else {
+                $message = 'Oops! Something went wrong...';
+            }
             $array = $this->settings['serializer']->serialize(new \Egg\Http\Error(array(
                 'name'        => 'server_error',
-                'description' => 'Oops! Something went wrong...',
+                'description' => $message,
                 'uri'         => '',
             )));
             $response->getBody()->setContent($array);
